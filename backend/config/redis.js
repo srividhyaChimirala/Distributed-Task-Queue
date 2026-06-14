d60eefaba607ecaeb1ab2be96555742e635690ec
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-06-14 20:17:45 +0530

    Update redis.js

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,9 +1,22 @@
+// import IORedis from "ioredis";
+
+// const connection = new IORedis({
+//   host: "127.0.0.1",
+//   port: 6379,
+//   maxRetriesPerRequest: null,
+// });
+
+// export default connection;
 import IORedis from "ioredis";
 
-const connection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null,
-});
+const connection = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+    })
+  : new IORedis({
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: process.env.REDIS_PORT || 6379,
+      maxRetriesPerRequest: null,
+    });
 
 export default connection;
