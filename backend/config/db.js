b8091e91c49fa2bb68cc1d138907ec920ec6500b
srividhyaChimirala <srividhyachimirala@gmail.com>
2026-06-14 21:23:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,33 +1,33 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log("MongoDB Connection Error:", error.message);
-
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-
-
-
-
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log("MONGO_URI exists:", !!uri);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB Connection Error:", error.message);
 
-  if (!uri) throw new Error("MONGO_URI not defined");
-
-  await mongoose.connect(uri);
-
-  console.log("MongoDB connected");
+    process.exit(1);
+  }
 };
+
+export default connectDB;
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   const uri = process.env.MONGO_URI;
+
+//   console.log("MONGO_URI exists:", !!uri);
+
+//   if (!uri) throw new Error("MONGO_URI not defined");
+
+//   await mongoose.connect(uri);
+
+//   console.log("MongoDB connected");
+// };
